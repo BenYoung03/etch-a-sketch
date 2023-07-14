065bfd43ce9906e34b867d89d6696a37d5f208ab
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-14 14:24:29 -0400

    Remove individual cell borders to enhance etch-a-sketch effect

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let gridSize = 16;
 function createGrid(gridSize) {
     for (i = 0; i < gridSize * gridSize; i++) {
         let square = document.createElement('div');
-        square.style.width = `${445 / gridSize}px`;
-        square.style.height = `${445  / gridSize}px`;
+        square.style.width = `${500 / gridSize}px`;
+        square.style.height = `${500 / gridSize}px`;
         square.classList.add('square');
         container.appendChild(square);
     }
