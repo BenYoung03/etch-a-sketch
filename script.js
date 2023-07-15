4dd0199ce81508ed257281fc1bb2d3ba7f8df108
BenYoung03 <bryoung1@lakeheadu.ca>
2023-07-15 00:43:38 -0400

    Add Eraser and Pencil button to toggle black and white pencil colour

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let container = document.querySelector('.container');
 let gridSize = 16;
+let colour = "black";
 
 function createGrid(gridSize) {
     for (i = 0; i < gridSize * gridSize; i++) {
         let square = document.createElement('div');
         square.style.width = `${500 / gridSize}px`;
         square.style.height = `${500 / gridSize}px`;
+        square.style.backgroundColor = 'white';
         square.classList.add('square');
         container.appendChild(square);
     }
@@ -17,12 +19,12 @@ const cells = document.querySelectorAll('.square');
 
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'black';
+        e.target.style.backgroundColor = colour;
     });
 });
 
-const button = document.getElementById('change-grid');
-button.addEventListener('click', () => {
+const changeBtn = document.getElementById('change-grid');
+changeBtn.addEventListener('click', () => {
     gridSize = prompt('Enter a number between 1 and 100');
     if (gridSize > 100 || gridSize < 1) {
         alert('Please enter a number between 1 and 100');
@@ -35,7 +37,31 @@ button.addEventListener('click', () => {
     const cells = document.querySelectorAll('.square');
     cells.forEach((cell) => {
         cell.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
+            e.target.style.backgroundColor = colour;
         });
     });
+});
+
+const clearBtn = document.getElementById('clear-grid');
+clearBtn.addEventListener('click', () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(gridSize);
+    const cells = document.querySelectorAll('.square');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = colour;
+        });
+    });
+});
+
+const eraser = document.getElementById('eraser');
+eraser.addEventListener('click', () => {
+    colour = 'white';
+});
+
+const pencil = document.getElementById('pencil');
+pencil.addEventListener('click', () => {
+    colour = 'black';
 });
